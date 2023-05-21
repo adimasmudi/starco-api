92f33b94a48f4968fb5ee7b6f405586dd4cf337f
adimasmudi <adimasmudi@gmail.com>
2023-05-21 11:57:30 +0700

    add respon formatter for campaign

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,39 @@
 package campaign
+
+type CampaignFormatter struct {
+	Id               int    `json:"id"`
+	UserId           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	CampaignFormatter := CampaignFormatter{}
+	CampaignFormatter.Id = campaign.ID
+	CampaignFormatter.UserId = campaign.UserId
+	CampaignFormatter.Name = campaign.Name
+	CampaignFormatter.ShortDescription = campaign.ShortDescription
+	CampaignFormatter.GoalAmount = campaign.GoalAmount
+	CampaignFormatter.CurrentAmount = campaign.CurrentAmount
+	CampaignFormatter.ImageUrl = ""
+
+	if len(campaign.CampaignImages) > 0 {
+		CampaignFormatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+
+	return CampaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	var campaignsFormatter []CampaignFormatter
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
